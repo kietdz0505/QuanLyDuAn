9fca8993862ce81522f1b00f322d80f2e022c39b
Loc <locn190904@gmail.com>
2025-07-07 10:10:32 +0700

    xay dung model user

---
+++ ThuVienOU/libraryapp/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import cloudinary
+from urllib.parse import quote
+
+app = Flask(__name__)
+app.secret_key = '@dhuhuigugsusrbfberbđsdfghjkjghrg4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/librarydb?charset=utf8mb4' % quote("Admin@123")
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = True
+app.config['PAGE_SIZE'] = 8
+app.config['COMMENT_SIZE'] = 20
+
+
+db = SQLAlchemy(app=app)
+
+cloudinary.config(
+    cloud_name = 'dfgnoyf71',
+    api_key = '993678569624556',
+    api_secret ='coCHzmBGd-KVX1ZJxbecKfrETog',
+)
+
+login = LoginManager(app=app)
---
+++ ThuVienOU/libraryapp/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Boolean, Enum, ForeignKey, Integer, String, Float, DateTime, Text
+from sqlalchemy.orm import relationship, backref
+from libraryapp import db, app
+from datetime import datetime
+from flask_login import UserMixin
+from enum import Enum as UserEnum
+
+class BaseModel(db.Model):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+class UserRole(UserEnum):
+    ADMIN = 1
+    USER = 2
+
+class User(BaseModel, UserMixin):
+    name = Column(String(50), nullable=False)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
+    avatar = Column(String(100))
+    email = Column(String(50))
+    active = Column(Boolean, default=True)
+    joined_date = Column(DateTime, default=datetime.now())
+    user_role = Column(Enum(UserRole), default=UserRole.USER)
+
+    def __str__(self):
+        return self.name
