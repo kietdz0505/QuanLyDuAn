151d0526198159ba18470ab9fa3c85dde390152c
kietdz0505 <tgkiet03@gmail.com>
2025-09-23 13:54:26 +0700

    cover secret

--- ThuVienOU/libraryapp/__init__.py
+++ ThuVienOU/libraryapp/__init__.py
@@ -11,13 +11,11 @@ mail = Mail()
 
 def create_app():
     app = Flask(__name__, template_folder='../templates')
-    app.secret_key = '@dhuhuigugsusrbfberbsdfghjkjghrg4'
+    app.secret_key = 'YOUR_SECRET_KEY'  # Thay bằng một chuỗi bí mật thực sự
 
     # Database config
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
-        'mysql+pymysql://root:%s@localhost/librarydb?charset=utf8mb4'
-        % quote("Admin@123")
-    )
+    app.config['SQLALCHEMY_DATABASE_URI'] = ({'YOUR_DB_USER'} + ':' +
+                                             quote('YOUR_DB_PASSWORD') + '@localhost/librarydb?charset=utf8mb4')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # App config
@@ -29,15 +27,15 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'gioighu@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'dkwm rrhs puak jirr'  # dùng App Password của Gmail
-    app.config['MAIL_DEFAULT_SENDER'] = 'gioighu@gmail.com'
+    app.config['MAIL_USERNAME'] = ''
+    app.config['MAIL_PASSWORD'] = ''  # dùng App Password của Gmail
+    app.config['MAIL_DEFAULT_SENDER'] = ''
 
     # Cloudinary config
     cloudinary.config(
-        cloud_name='dfgnoyf71',
-        api_key='993678569624556',
-        api_secret='coCHzmBGd-KVX1ZJxbecKfrETog',
+        cloud_name='YOUR_CLOUD_NAME',
+        api_key='YOUR_API_KEY',
+        api_secret='YOUR_API_SECRET',
     )
 
     # Init extensions với app
